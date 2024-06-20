be8616ffc6788ed675238168fe0f80d9d388c825
ksanand-ux <72147791+ksanand-ux@users.noreply.github.com>
2024-06-20 14:15:54 +0530

    setup.py added.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Yolov8_object_detection',
+    version= '0.0.0',
+    author= 'Kumar Anand',
+    author_email= 'helloksanand86@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
